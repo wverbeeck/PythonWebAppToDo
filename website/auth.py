e0110a50358c26e7ea996921e61c6b7d0086983f
wouter <wouter@debianBig1.mshome>
2022-03-26 16:52:02 +0100

    forms ready

--- website/auth.py
+++ website/auth.py
@@ -1,9 +1,13 @@
-from flask import Blueprint, render_template
+from unicodedata import category
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
+    data = request.form
+    print(data)
+    
     return render_template("login.html", boolean=True)
     #TODO: other examples: text="Testing", user="Wouter"
     
@@ -12,6 +16,25 @@ def login():
 def logout():
         return "<p>logout</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def signup():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than 4 characters', category='error')
+            
+        elif len(firstName) < 2:
+            flash('First Name must be greater than 1 character', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match', category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters', category='error')
+        else:
+            #add user to database
+            flash('Account created!', category = 'success')
+
     return render_template("signup.html")
