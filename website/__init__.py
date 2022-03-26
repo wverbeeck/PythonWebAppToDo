6f3f21e4ea54a8cb97e5786e5f90a92ddff4185c
wouter <wouter@debianBig1.mshome>
2022-03-26 17:17:40 +0100

    db creation script entered

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from os import path
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -18,5 +18,16 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    from .models import User, Note  # or just state 'import .models' -> this latter doesn't work in this context. may want to investigate why
+  
+
+    #create db if needed
+    create_database(app)
+
     return app
-    
+
+#check if db already exists, if not: create it    
+def create_database (app):
+    if not path.exists('website/' + DB_NAME ):
+        db.create_all(app=app)
+        print('DAtabase created!')
--- website/models.py
+++ website/models.py
@@ -1,1 +1,19 @@
 from . import db 
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    iser_id = db.Column(db.Integer, db.ForeignKey('user.id')) 
+    #by creating User class, a table with 'user' will be created for reference. hence the lower case 'user.id'
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
+    notes = db.relationship('Note') #in this case, for the relationship, a Capital first letter is needed
+
